24fa4d390e476cfe976f1ec911acc1fa6035ecab
devbotbo <7118.eth@proton.me>
2025-08-02 23:47:27 +0200

    Add scripts/abis directory

---
+++ scripts/abis/index.ts
@@ -0,0 +1,19 @@
+// Auto-generated ABI exports
+import SimpleEscrowABI from "./SimpleEscrow.json" assert { type: "json" };
+import SimpleEscrowFactoryABI from "./SimpleEscrowFactory.json" assert { type: "json" };
+import MockERC20ABI from "./MockERC20.json" assert { type: "json" };
+import OneInchAdapterABI from "./OneInchAdapter.json" assert { type: "json" };
+import LightningBridgeABI from "./LightningBridge.json" assert { type: "json" };
+
+export {
+  SimpleEscrowABI,
+  SimpleEscrowFactoryABI,
+  MockERC20ABI,
+  OneInchAdapterABI,
+  LightningBridgeABI,
+};
+
+// Convenience exports
+export const ERC20_ABI = MockERC20ABI;
+export const FACTORY_ABI = SimpleEscrowFactoryABI;
+export const ESCROW_ABI = SimpleEscrowABI;
